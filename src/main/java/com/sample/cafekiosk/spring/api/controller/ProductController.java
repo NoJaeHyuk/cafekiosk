199e3b4322e43b30b4b595b1c1a14760abde5d00
jaehyuk <jaehyuk7722@naver.com>
2023-12-14 23:37:21 +0900

    feat: 판매중인 상품 호출 API 구현
    - 판매중인 상품 호출 API 구현 사이클 생성

---
+++ src/main/java/com/sample/cafekiosk/spring/api/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.sample.cafekiosk.spring.api.controller;
+
+import com.sample.cafekiosk.spring.api.service.product.ProductService;
+import com.sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/api/v1/products/selling")
+    public List<ProductResponse> getSellingProducts() {
+        return productService.getSellingProducts();
+    }
+
+}
